a5cab8fc357dd40a01c5c96b55c46db2f7707b65
Tiago Lima <tiagolima@osf.digital>
2026-05-12 15:49:47 -0300

    feat: added the cancel subscription payment with stripe

--- app/dashboard/minha-assinatura/page.tsx
+++ app/dashboard/minha-assinatura/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import {
 	Card,
 	CardContent,
@@ -5,21 +6,42 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { fetchSubscriptionByEmail } from "@/lib/stripe";
 import { CreditCard, XCircle } from "lucide-react";
+import Form from "next/form";
+import cancelSubscriptionAction from "./cancel-subscription-action";
+import BannerWarning from "@/components/banner-warning";
+import PricingCard from "@/components/pricing-card";
 
 export default async function MySubscription() {
+	const session = await auth();
+	let subscription = null;
+
+	if (session?.user?.email) {
+		const email = session?.user?.email as string;
+		subscription = await fetchSubscriptionByEmail(email);
+	}
+
 	return (
 		<>
 			<h1 className="text-3xl font-bold mb-6">Minha Assinatura</h1>
-			<div className="flex gap-10">
-				<PlanCard />
-				<ActionCard />
-			</div>
+			{subscription && (
+				<div className="flex gap-10">
+					<PlanCard subscription={subscription} />
+					<ActionCard subscription={subscription} />
+				</div>
+			)}
+			{!subscription && (
+				<>
+					<BannerWarning text="Você precisa de uma assinatura ativa. Quer tal assinar agora?" />
+					<PricingCard />
+				</>
+			)}
 		</>
 	);
 }
 
-function PlanCard() {
+function PlanCard({ subscription }: { subscription: any }) {
 	return (
 		<Card className="max-w-md w-full">
 			<CardHeader>
@@ -30,23 +52,32 @@ function PlanCard() {
 				<div className="space-y-3">
 					<div className="flex justify-between">
 						<span className="text-gray-600">Plano:</span>
-						<span>Plano Pro</span>
+						<span>{subscription.plan.nickname}</span>
 					</div>
 					<div className="flex justify-between">
 						<span className="text-gray-600">Status:</span>
-						<span className="text-green-600">Ativo</span>
+						<span className="text-green-600">{subscription.status}</span>
 					</div>
 					<div className="flex justify-between">
 						<span className="text-gray-600">Próxima cobrança:</span>
-						<span>01/01/2025</span>
+						<span>
+							{new Date(
+								subscription.current_period_end * 1000,
+							).toLocaleDateString("pt-BR")}
+						</span>
 					</div>
 					<div className="flex justify-between">
 						<span className="text-gray-600">Valor:</span>
-						<span>R$ 29,00</span>
+						<span>
+							{(subscription.plan.amount / 100).toLocaleString("pt-BR", {
+								style: "currency",
+								currency: "BRL",
+							})}
+						</span>
 					</div>
 					<div className="flex justify-between">
 						<span className="text-gray-600">Ciclo:</span>
-						<span>Mensal</span>
+						<span>{subscription.plan.interval}</span>
 					</div>
 				</div>
 			</CardContent>
@@ -54,7 +85,7 @@ function PlanCard() {
 	);
 }
 
-function ActionCard() {
+function ActionCard({ subscription }: { subscription: any }) {
 	return (
 		<Card className="w-full max-w-sm h-full">
 			<CardHeader>
@@ -67,10 +98,17 @@ function ActionCard() {
 						<CreditCard className="mr-2 h-5 w-5 text-gray-400" />
 						Atualizar método de pagamento
 					</button>
-					<button className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-						<XCircle className="mr-2 h-5 w-5" />
-						Cancelar assinatura
-					</button>
+					<Form action={cancelSubscriptionAction}>
+						<input
+							type="hidden"
+							name="subscriptionId"
+							value={subscription.id}
+						/>
+						<button className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+							<XCircle className="mr-2 h-5 w-5" />
+							Cancelar assinatura
+						</button>
+					</Form>
 				</div>
 			</CardContent>
 		</Card>
